eac7e9940415e9dd18dfb9fddaeb94b5a7213253
Mateus Gomes <mateusgomes.nascimento21@gmail.com>
2022-08-08 11:23:46 -0300

    feat: create dataset

--- src/App.js
+++ src/App.js
@@ -1,8 +1,56 @@
 
 function App() {
+  const data = [
+    [
+      {
+        id: 1,
+        name: 'John',
+        age: 25
+      },
+      {
+        id: 2,
+        name: 'Maria',
+        age: 30
+      }
+    ],
+    [
+      {
+        id: 1,
+        name: 'Marcus',
+        age: 18
+      },
+      {
+        id: 2,
+        name: 'Jessica',
+        age: 23
+      },
+      {
+        id: 3,
+        name: 'Matt',
+        age: 22
+      }
+    ]
+  ]
+
   return (
    <div>
     <h1>React Export Excel</h1>
+    {data.map((arr,i) => (
+      <>
+        <h2>Person Set {i+1}</h2>
+        <ul>
+          {arr.map(e => (
+            <div>
+              <li>id: {e.id}</li>
+              <li>name: {e.name}</li>
+              <li>age: {e.age}</li>
+              <br></br>
+            </div>
+          ))}
+        </ul>
+      </>
+    ))}
+    <button onClick={()=>console.log('Exporting...')}>Export</button>
    </div>
   );
 }
